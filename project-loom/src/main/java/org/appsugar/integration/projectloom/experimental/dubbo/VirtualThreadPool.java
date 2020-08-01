8744c69bd1b4b5f153fdf51fdf0725496beb6dd1
newyoung <shen.ly@vivebest.com>
2020-08-01 22:02:49 +0800

    add dubbo virtual thread support

--- project-loom/src/main/java/org/appsugar/integration/projectloom/experimental/dubbo/VirtualThreadPool.java
+++ project-loom/src/main/java/org/appsugar/integration/projectloom/experimental/dubbo/VirtualThreadPool.java
@@ -4,30 +4,14 @@ import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.threadpool.ThreadPool;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * 虚拟线程池
  */
 public class VirtualThreadPool implements ThreadPool {
-
-    private Executor executor = new VirtualThreadExecutor();
-
     @Override
     public Executor getExecutor(URL url) {
-        return executor;
-    }
-}
-
-class VirtualThreadExecutor implements Executor {
-
-    @Override
-    public void execute(Runnable command) {
-        Thread current = Thread.currentThread();
-        if (current.isVirtual()) {
-            command.run();
-            return;
-        }
-        Thread builder = Thread.builder().name("VirtualThread-" + current.getName()).inheritThreadLocals().virtual().task(command).build();
-        builder.start();
+        return Executors.newUnboundedVirtualThreadExecutor();
     }
 }
--- project-loom/src/main/java/org/appsugar/integration/projectloom/experimental/spring/webflux/embedded/VirtualThreadWebFilter.java
+++ project-loom/src/main/java/org/appsugar/integration/projectloom/experimental/spring/webflux/embedded/VirtualThreadWebFilter.java
@@ -1,22 +1,24 @@
 package org.appsugar.integration.projectloom.experimental.spring.webflux.embedded;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 /**
  * 使用WebFlux时,转发所有请求到轻量级线程中
  */
 public class VirtualThreadWebFilter implements WebFilter {
-    @Autowired
-    private VirtualThreadExecutor virtualThreadExecutor;
+
+    private Executor execute = Executors.newUnboundedVirtualThreadExecutor();
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         return Mono.create(sink -> {
-            virtualThreadExecutor.execute(() -> chain.filter(exchange)
+            execute.execute(() -> chain.filter(exchange)
                     .subscriberContext(sink.currentContext())
                     .subscribe(r -> sink.success(), r -> sink.error(r)));
         });
